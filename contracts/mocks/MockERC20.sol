994d55a9a535f1288fd2a8455854778693eafdea
nikitavorobievvvv-cpu <nikitavorobievvvv@gmail.com>
2025-11-05 15:05:31 +0100

    test(mocks): rename immutable to SNAKE_CASE to satisfy solhint

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    uint8 private immutable _decimals;
+    uint8 private immutable DECIMALS_; // solhint-disable-line immutable-vars-naming
 
     constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
-        _decimals = decimals_;
+        DECIMALS_ = decimals_;
     }
 
     function decimals() public view override returns (uint8) {
-        return _decimals;
+        return DECIMALS_;
     }
 
     function mint(address to, uint256 amount) external {
