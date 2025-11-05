435c15d0d8da182b920ce5451515e80134d7df11
nikitavorobievvvv-cpu <nikitavorobievvvv@gmail.com>
2025-11-05 15:12:11 +0100

    Update MockERC20.sol

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    uint8 private immutable DECIMALS_; // solhint-disable-line immutable-vars-naming
+    uint8 private immutable DECIMALS;
 
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
-        DECIMALS_ = decimals_;
+    constructor(string memory name_, string memory symbol_, uint8 decimals_)
+        ERC20(name_, symbol_)
+    {
+        DECIMALS = decimals_;
     }
 
     function decimals() public view override returns (uint8) {
-        return DECIMALS_;
+        return DECIMALS;
     }
 
     function mint(address to, uint256 amount) external {
